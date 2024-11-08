8959edcccccbd0cd72130271b3c954fffb3137d1
ahmad <zahooraup334@gmail.com>
2024-11-08 15:46:40 +0500

    Initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,8 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ecommerce_app/main.dart';
-
+import 'package:commerce_store/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
